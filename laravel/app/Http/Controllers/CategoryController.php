2d9092e280a8bb82cb1b964e3ecb5ef0f21d6d1c
yumekiti <yumekiti120440@gmail.com>
2021-07-22 19:44:28 +0900

    fix: category

--- laravel/app/Http/Controllers/CategoryController.php
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -75,8 +75,6 @@ class CategoryController extends Controller
     public function create(Request $req){
         
         $category = Auth::user()->
-        devices()->
-        firstOrFail()->
         categories()->
         create([
             'category_name' => $req->input('category_name'),
